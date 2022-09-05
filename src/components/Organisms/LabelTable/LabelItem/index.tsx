54e8a56e5c006deda57e74be211a331789eebafa
연 <mogoo22@naver.com>
2022-09-05 12:21:34 +0900

    ♻️ LabelTable에서 LabelItem 컴포넌트 분리를 하위디렉토리로 분리함

---
+++ src/components/Organisms/LabelTable/LabelItem/index.tsx
@@ -0,0 +1,61 @@
+import Button from '@/components/Atoms/Button';
+import Label from '@/components/Atoms/Label';
+import * as S from '@/components/Organisms/LabelTable/LabelItem/index.styled';
+import { COLORS } from '@/styles/theme';
+import { LabelState, LabelTypes } from '@/stores/labelList';
+import { useNavigate } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+import { ModalState } from '@/components/Modal';
+
+const LabelItem = ({ id, title, backgroundColorCode, description, textColor }: LabelTypes) => {
+  const navigate = useNavigate();
+  const setLabelState = useSetRecoilState(LabelState);
+  const setIsModal = useSetRecoilState(ModalState);
+
+  const handleEditButtonClick = (props: LabelTypes) => {
+    setLabelState({ type: 'EDIT', label: props });
+  };
+  const handleDeleteButtonClick = (deletedLabelId: number) => {
+    setLabelState((prev) => ({ type: 'DELETE', label: { ...prev.label, id: deletedLabelId } }));
+    setIsModal(true);
+  };
+  const handleLabelClick = (filterdLabelTitle: string) => {
+    navigate(`/issues?q=label%3A"${filterdLabelTitle}"`);
+  };
+
+  return (
+    <S.LabelItem>
+      <Label
+        title={title}
+        backgroundColor={backgroundColorCode}
+        textColor={textColor}
+        onClick={() => handleLabelClick(title)}
+      />
+      <S.Description>{description}</S.Description>
+      <S.EditButton>
+        <Button
+          buttonStyle="NO_BORDER"
+          iconInfo={{
+            icon: 'Edit',
+            stroke: COLORS.LABEL,
+          }}
+          label="편집"
+          size="SMALL"
+          handleOnClick={() => handleEditButtonClick({ id, title, backgroundColorCode, description, textColor })}
+        />
+        <Button
+          buttonStyle="NO_BORDER"
+          iconInfo={{
+            icon: 'Trash',
+            stroke: COLORS.ERROR.RED,
+          }}
+          label="삭제"
+          size="SMALL"
+          handleOnClick={() => handleDeleteButtonClick(id)}
+        />
+      </S.EditButton>
+    </S.LabelItem>
+  );
+};
+
+export default LabelItem;
