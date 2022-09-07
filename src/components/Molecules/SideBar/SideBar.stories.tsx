b6636c56fe268d54dac5b689b3300fee96b784dd
jm <jm.dev.study@gmail.com>
2022-09-07 10:57:58 +0900

    🤡 SideBar와 관련된 mock data를 파일로 분리

--- src/components/Molecules/SideBar/SideBar.stories.tsx
+++ src/components/Molecules/SideBar/SideBar.stories.tsx
@@ -1,18 +1,18 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import SideBar from '@/components/Molecules/SideBar';
-import { LABEL_LIST, MILESTONE_LIST, USER_LIST } from '@/components/Molecules/Dropdown/mocks';
-import { ContentListTypes, SideBarItemType } from '@/components/Molecules/SideBar/types';
+import { SideBarItemType } from '@/components/Molecules/SideBar/types';
 
+import { useQuery } from '@tanstack/react-query';
 import useFetchMilestone from '@/hooks/useFetchMilestone';
 import useLabelFetch from '@/hooks/useLabelFetch';
+import { getMemberData } from '@/api/login_logout';
+import { UserTypes } from '@/components/Molecules/Dropdown/types';
 
 import { milestoneHandlers } from '@/mocks/handlers/milestones';
 import { labelHandlers } from '@/mocks/handlers/label';
-
-import { useQuery } from '@tanstack/react-query';
-import { getMemberData } from '@/api/login_logout';
-import { UserTypes } from '@/components/Molecules/Dropdown/types';
 import { authHandlers } from '@/mocks/handlers/auth';
+
+import { DEFAULT_CONTENT_LIST, MOCK_CONTENT_LIST, SIDEBAR_PROPS } from '@/components/Molecules/SideBar/mock';
 
 export default {
   title: 'Molecules/SideBar',
@@ -25,13 +25,7 @@ const Content = () => {
   const { data: labelList } = useGetLabel();
   const { data: userList } = useQuery<UserTypes[]>(['users'], getMemberData);
 
-  const emptyContentList: ContentListTypes = {
-    assignee: [],
-    label: [],
-    milestone: [],
-  };
-
-  const SIDEBAR_PROPS: SideBarItemType[] = [
+  const MOCK_SIDEBAR_PROPS: SideBarItemType[] = [
     {
       id: 'assignee',
       dropdownTitle: '담당자',
@@ -56,61 +50,13 @@ const Content = () => {
   ];
   return (
     <div>
-      <SideBar sideBarList={SIDEBAR_PROPS} content={emptyContentList} />
+      <SideBar sideBarList={MOCK_SIDEBAR_PROPS} content={DEFAULT_CONTENT_LIST} />
     </div>
   );
 };
 
 const Template: ComponentStory<typeof Content> = () => <Content />;
 const MockTemplate: ComponentStory<typeof SideBar> = (args) => <SideBar {...args} />;
-
-const contentList: ContentListTypes = {
-  assignee: [],
-  label: [
-    {
-      id: 0,
-      title: 'feature',
-      backgroundColorCode: '#007AFF',
-      description: '',
-      textColor: 'BLACK',
-    },
-  ],
-  milestone: [
-    {
-      id: 0,
-      title: '마일스톤 1',
-      description: null,
-      dueDate: null,
-      closed: false,
-      openIssueCount: 3,
-      closedIssueCount: 7,
-    },
-  ],
-};
-
-const SIDEBAR_PROPS: SideBarItemType[] = [
-  {
-    id: 'assignee',
-    dropdownTitle: '담당자',
-    dropdownListTitle: '담당자 필터',
-    dropdownList: USER_LIST,
-    dropdownType: 'checkbox',
-  },
-  {
-    id: 'label',
-    dropdownTitle: '레이블',
-    dropdownListTitle: '레이블 필터',
-    dropdownList: LABEL_LIST,
-    dropdownType: 'checkbox',
-  },
-  {
-    id: 'milestone',
-    dropdownTitle: '마일스톤',
-    dropdownListTitle: '마일스톤 필터',
-    dropdownList: MILESTONE_LIST,
-    dropdownType: 'radio',
-  },
-];
 
 export const Initial = Template.bind({});
 Initial.parameters = {
@@ -122,5 +68,5 @@ Initial.parameters = {
 export const Checked = MockTemplate.bind({});
 Checked.args = {
   sideBarList: SIDEBAR_PROPS,
-  content: contentList,
+  content: MOCK_CONTENT_LIST,
 };
