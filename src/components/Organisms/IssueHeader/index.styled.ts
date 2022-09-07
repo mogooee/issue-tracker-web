0df41577e228dda352c011b564ad2fddf015a95a
연 <mogoo22@naver.com>
2022-09-07 12:22:15 +0900

    ♻️ IssueHeader의 HeaderInline 컴포넌트 분리

--- src/components/Organisms/IssueHeader/index.styled.ts
+++ src/components/Organisms/IssueHeader/index.styled.ts
@@ -6,29 +6,6 @@ export const HeaderInline = styled.div`
   margin-bottom: 16px;
 `;
 
-export const Title = styled.div`
-  ${({ theme }) => theme.MIXIN.FLEX({ align: 'center', justify: 'flex-start' })};
-  ${({ theme }) => theme.FONTSTYLES.DISPLAY_REGULER};
-  width: 940px;
-  height: 48px;
-
-  form {
-    width: inherit;
-  }
-
-  .issueNumber {
-    margin-left: 16px;
-    color: ${({ theme }) => theme.COLORS.LABEL};
-  }
-`;
-
-export const ButtonTab = styled.div`
-  ${({ theme }) => theme.MIXIN.FLEX({ align: 'center', justify: 'center' })};
-  button + button {
-    margin-left: 8px;
-  }
-`;
-
 export const Info = styled.div<{ closed: boolean }>`
   ${({ theme }) => theme.MIXIN.FLEX({ align: 'center', justify: 'flex-start' })};
   gap: 8px;
