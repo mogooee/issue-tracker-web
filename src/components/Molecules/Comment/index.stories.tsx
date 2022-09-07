d925f1c54c8db9a38888d8049061c519b0eef1ea
연 <mogoo22@naver.com>
2022-09-07 11:24:33 +0900

    🤡 Comment story에 Reaction 조회 모킹 API 연결

--- src/components/Molecules/Comment/index.stories.tsx
+++ src/components/Molecules/Comment/index.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Comment from '@/components/Molecules/Comment';
 import { comment } from '@/mocks/tables/issue';
+import { issueHandlers } from '@/mocks/handlers/issue';
 
 export default {
   title: 'Molecules/Comment',
@@ -11,9 +12,24 @@ const Template: ComponentStory<typeof Comment> = (args) => <Comment {...args} />
 
 export const Author = Template.bind({});
 Author.args = { isAuthor: true, comment: { ...comment, issueCommentReactionsResponse: [] } };
+Author.parameters = {
+  msw: {
+    handlers: issueHandlers,
+  },
+};
 
 export const NoAuthor = Template.bind({});
 NoAuthor.args = { ...Author.args, isAuthor: false };
+NoAuthor.parameters = {
+  msw: {
+    handlers: issueHandlers,
+  },
+};
 
 export const HasReaction = Template.bind({});
 HasReaction.args = { ...Author.args, comment };
+HasReaction.parameters = {
+  msw: {
+    handlers: issueHandlers,
+  },
+};
