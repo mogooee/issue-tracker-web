e76804b5679761e35a1d7740cee409bd71b65e61
jm <jm.dev.study@gmail.com>
2022-09-12 02:14:43 +0900

    💄✨ 이슈 작성 취소 모달 컴포넌트 구현 및 기능 적용 - 이슈 작성 페이지에서 작성 취소 버튼을 누르면 해당 모달을 보여준다. - 모달에서 확인 버튼 클릭시 폼을 리셋하고 이슈 페이지로 이동한다.

--- src/pages/Private/NewIssue/index.tsx
+++ src/pages/Private/NewIssue/index.tsx
@@ -1,4 +1,4 @@
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { LoginUserInfoState } from '@/stores/loginUserInfo';
 
 import * as S from '@/pages/Private/NewIssue/index.styles';
@@ -12,38 +12,51 @@ import { DEFAULT_CONTENT_LIST, SIDEBAR_PROPS } from '@/components/Molecules/Side
 import useInput from '@/hooks/useInput';
 import { NEW_ISSUE_BUTTON_INFO } from '@/components/Atoms/Button/options';
 
+import Modal, { ModalState } from '@/components/Modal';
+import CancelNewIssueModal from '@/components/Modal/CancelNewIssue';
+
 const NewIssue = () => {
   const LoginUserInfoStateValue = useRecoilValue(LoginUserInfoState);
   const { isActive, isTyping, onChangeInput, onClickInput, onBlurInput } = useInput();
+  const [isOpenModal, setIsOpenModal] = useRecoilState(ModalState);
+
+  const OnClickCancelButton = () => setIsOpenModal(true);
 
   return (
-    <S.NewIssue>
-      <h1>새로운 이슈 작성</h1>
-      <S.Divider />
-      <S.NewIssueEditer>
-        <UserImage imgSize="MEDIUM" {...LoginUserInfoStateValue} />
-        <S.NewIssueForm isActive={isActive}>
-          <Input
-            inputMaxLength={255}
-            inputPlaceholder="제목"
-            inputSize="MEDIUM"
-            inputType="text"
-            isActive={isActive}
-            isTyping={isTyping}
-            onChange={onChangeInput}
-            onClick={onClickInput}
-            onBlur={onBlurInput}
-          />
-          <TextAreaEditer />
-        </S.NewIssueForm>
-        <SideBar content={DEFAULT_CONTENT_LIST} sideBarList={SIDEBAR_PROPS} />
-      </S.NewIssueEditer>
-      <S.Divider />
-      <S.NewIssueButtons>
-        <Button {...NEW_ISSUE_BUTTON_INFO.CANCEL} />
-        <Button {...NEW_ISSUE_BUTTON_INFO.COMPLETE} />
-      </S.NewIssueButtons>
-    </S.NewIssue>
+    <>
+      <S.NewIssue>
+        <h1>새로운 이슈 작성</h1>
+        <S.Divider />
+        <S.NewIssueEditer>
+          <UserImage imgSize="MEDIUM" {...LoginUserInfoStateValue} />
+          <S.NewIssueForm isActive={isActive}>
+            <Input
+              inputMaxLength={255}
+              inputPlaceholder="제목"
+              inputSize="MEDIUM"
+              inputType="text"
+              isActive={isActive}
+              isTyping={isTyping}
+              onChange={onChangeInput}
+              onClick={onClickInput}
+              onBlur={onBlurInput}
+            />
+            <TextAreaEditer />
+          </S.NewIssueForm>
+          <SideBar content={DEFAULT_CONTENT_LIST} sideBarList={SIDEBAR_PROPS} />
+        </S.NewIssueEditer>
+        <S.Divider />
+        <S.NewIssueButtons>
+          <Button {...NEW_ISSUE_BUTTON_INFO.CANCEL} handleOnClick={OnClickCancelButton} />
+          <Button {...NEW_ISSUE_BUTTON_INFO.COMPLETE} />
+        </S.NewIssueButtons>
+      </S.NewIssue>
+      {isOpenModal && (
+        <Modal>
+          <CancelNewIssueModal />
+        </Modal>
+      )}
+    </>
   );
 };
 
